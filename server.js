7adffa2b5459cde8311211d92a2230852e3512b4
Paula <beltag.paula@gmail.com>
2026-09-11 16:27:58 +0300

    3rd commit, functionable

--- server.js
+++ server.js
@@ -9,33 +9,137 @@ const {
     downloadVideo,
     downloadAudio,
     downloadThumbnail,
-    getCaptions,
     downloadCaption
 } = require('./services/ytdlp');
 
 
-app.use(express.urlencoded({ extended: true }));   
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.get('/', (req, res) => {
-  res.render('index', { info: null});
+    res.render('index', {
+        info: null
+    });
 });
 
-app.post('/download', async (req, res)=>{
+app.post('/inspect', async (req, res) => {
+
     const url = req.body.url;
 
-    try{
+    try {
+
         const info = await inspectVideo(url);
 
-        res.render('index', {info});
-    }
-    catch(error){
+        console.log(info);
+
+        res.render('index', {
+            info,
+            url
+        });
+
+    } catch (error) {
+
         console.error(error);
-        res.status(500).send("Could not inspect/find this video")
+
+        res.status(500).send('Could not inspect video');
     }
-})
+});
+
+
+app.post('/download/video', async (req, res) => {
+
+    const url = req.body.url;
+    const quality = req.body.quality;
+
+    try {
+
+        const file = await downloadVideo(url, quality);
+
+        console.log('Video downloaded:', file);
+
+        res.download(file);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).send('Could not download video');
+    }
+});
+
+
+app.post('/download/audio', async (req, res) => {
+
+    const url = req.body.url;
+    const quality = req.body.quality;
+
+    try {
+
+        const file = await downloadAudio(url, quality);
+
+        console.log('Audio downloaded:', file);
+
+        res.download(file);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).send('Could not download audio');
+    }
+});
+
+
+app.post('/download/caption', async (req, res) => {
+
+    const url = req.body.url;
+    const [language, type] = req.body.language.split('|');
+
+    try {
+
+        const file = await downloadCaption(
+            url,
+            language,
+            type
+        );
+
+        console.log('Caption downloaded:', file);
+
+        res.download(file);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).send('Could not download caption');
+    }
+});
+
+
+app.post('/download/thumbnail', async (req, res) => {
+
+    const url = req.body.url;
+
+    try {
+
+        const file = await downloadThumbnail(url);
+
+        console.log('Thumbnail downloaded:', file);
+
+        res.download(file);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).send('Could not download thumbnail');
+    }
+});
+
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
--- services/ytdlp.js
+++ services/ytdlp.js
@@ -1,7 +1,8 @@
-const { channel } = require('node:diagnostics_channel');
-const { YtDlp } = require('ytdlp-nodejs');
+const { YtDlp } = require('ytdlp-nodejs'); //https://www.npmjs.com/package/ytdlp-nodejs
+const fs = require('fs');
 
 const ytdlp = new YtDlp();
+
 
 //u get info about yt videos
 const inspectVideo = async (url) => {
@@ -22,13 +23,14 @@ const inspectVideo = async (url) => {
         )[0];
 
     const subtitles = Object.keys(info.subtitles || {});
+
     const automaticCaptions = Object.keys(
         info.automatic_captions || {}
     );
 
     return {
         title: info.title,
-        channel: info.channel,
+        channel: info.uploader,
         duration: info.duration,
         thumbnail: bestThumbnail?.url,
 
@@ -39,16 +41,158 @@ const inspectVideo = async (url) => {
         automaticCaptions
     };
 };
-//download video
-const downloadVideo = (url, quality) => { }
-//download audio
-const downloadAudio = (url, quality) => { }
+
+
+//download video based on 3 types of quality
+const downloadVideo = async (url, quality) => {
+
+    const qualities = {
+        low: '480p',
+        medium: '720p',
+        best: 'highest'
+    };
+
+    if (!qualities[quality]) {
+        throw new Error('Invalid video quality');
+    }
+
+    const result = await ytdlp.downloadAsync(url, {
+        format: {
+            filter: 'mergevideo',
+            quality: qualities[quality],
+            type: 'mp4'
+        },
+        output: './downloads/%(title)s.%(ext)s'
+    });
+
+    return result.filePaths[0];
+};
+
+//download audio based on 3 types of quality
+const downloadAudio = async (url, quality) => {
+
+    const qualities = {
+        low: 5,
+        medium: 3,
+        best: 1
+    };
+
+    if (!qualities[quality]) {
+        throw new Error('Invalid audio quality');
+    }
+
+    const result = await ytdlp.downloadAsync(url, {
+        format: {
+            filter: 'audioonly',
+            quality: qualities[quality],
+            type: 'mp3'
+        },
+        output: './downloads/%(title)s.%(ext)s'
+    });
+
+    return result.filePaths[0];
+};
+
+
 //download thumbnail
-const downloadThumbnail = (url) => { }
-//find available caption of that vide
-const getCaptions = (url) => { }
+const downloadThumbnail = async (url) => {
+
+    const info = await ytdlp.getInfoAsync(url);
+
+    const bestThumbnail = info.thumbnails
+        ?.filter(thumbnail => thumbnail.width && thumbnail.height)
+        .sort((a, b) =>
+            (b.width * b.height) - (a.width * a.height)
+        )[0];
+
+    if (!bestThumbnail) {
+        throw new Error('No thumbnail found');
+    }
+
+    const response = await fetch(bestThumbnail.url);
+
+    if (!response.ok) {
+        throw new Error('Could not download thumbnail');
+    }
+
+    const buffer = Buffer.from(
+        await response.arrayBuffer()
+    );
+
+    const filename = `./downloads/${info.id}.jpg`;
+
+    fs.writeFileSync(filename, buffer);
+
+    return filename;
+};
+
+
+//find available caption of that video
+const getCaptions = async (url) => {
+
+    const info = await ytdlp.getInfoAsync(url);
+
+    return {
+        subtitles: Object.keys(info.subtitles || {}),
+
+        automaticCaptions: Object.keys(
+            info.automatic_captions || {}
+        )
+    };
+};
+
+
 //after that use this to download selected caption
-const downloadCaption = (url, language) => { }
+const downloadCaption = async (url, language, type) => {
+
+    const options = {
+        output: './downloads/%(title)s.%(ext)s',
+        subLangs: [language],
+        convertSubs: 'srt',
+        skipDownload: true
+    };
+
+    if (type === 'subtitle') {
+        options.writeSubs = true;
+    }
+
+    if (type === 'automatic') {
+        options.writeAutoSubs = true;
+    }
+
+    if (!fs.existsSync('./downloads')) {
+        fs.mkdirSync('./downloads', { recursive: true });
+    }
+
+    const before = new Set(fs.readdirSync('./downloads'));
+
+    await ytdlp.downloadAsync(url, options);
+
+    const after = fs.readdirSync('./downloads');
+
+    const newFile = after.find(file =>
+        !before.has(file) && file.endsWith('.srt')
+    );
+
+    if (!newFile) {
+        throw new Error('Could not find downloaded caption file');
+    }
+
+    return `./downloads/${newFile}`;
+};
+
+
+//just wrote to see what is the full JSON and stored into output.txt, not useful
+/*const saveJsonToTxtNode = (jsonData, filename) => {
+    // 1. Convert JSON object to a formatted string
+    const jsonString = JSON.stringify(jsonData, null, 2);
+
+    // 2. Write directly to the file system
+    fs.writeFileSync(filename, jsonString, 'utf8');
+    console.log(`File saved as ${filename}`);
+}*/
+
+
 
 module.exports = {
     inspectVideo,
